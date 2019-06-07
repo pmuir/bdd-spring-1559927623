f7f1a72433e37951b22b1f0c3c58b7693cf6a494
Pete Muir <pmuir@bleepbleep.org.uk>
2019-06-07 18:13:57 +0100

    Initial import

---
+++ src/main/java/com/example/bddspring1559927623/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.bddspring1559927623;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
